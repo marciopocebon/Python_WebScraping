7dc5d928c6e5c065920425bb0ab05a3235d47d70
uppusaikiran <saikiran.vit@gmail.com>
2016-02-15 09:05:12 -0800

    Pushed at 10:35 15-feb-16

---
+++ mechanize/web_spider_with_mechanize.py
@@ -0,0 +1,25 @@
+import mechanize
+import urlparse
+import urllib
+
+url ="http://cnn.com"
+br= mechanize.Browser()
+urls = [url]
+visited =[url]
+while len(urls) >0:
+	try:
+		br.open(urls[0])
+		urls.pop(0)
+		for link in br.links():
+			newurl = urlparse.urljoin(link.base_url,link.url)
+			b1 = urlparse.urlparse(newurl).hostname
+			b2 = urlparse.urlparse(newurl).path
+			newurl = "http://"+b1+b2
+
+			if newurl not in visited and urlparse.urlparse(url).hostname in newurl:
+				urls.append(newurl)
+				visited.append(newurl)
+				print newurl
+	except:
+		print "error"
+		urls.pop(0)
