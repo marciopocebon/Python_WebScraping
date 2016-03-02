1e6020744acefdb75cc87cc19dbf735c11e9dd94
uppusaikiran <saikiran.vit@gmail.com>
2016-03-02 18:30:54 +0530

    2-3-2016

--- SCRAPY/1-3-2016/_9gag/_9gag/spiders/first.py
+++ SCRAPY/1-3-2016/_9gag/_9gag/spiders/first.py
@@ -8,24 +8,33 @@ class FirstSpider(scrapy.Spider):
     start_urls = (
         'http://www.9gag.com/',
     )
+    
     last_gag_id = None
     def parse(self, response):
+    	count = 0
         for article in response.xpath('//article'):
             gag_id = article.xpath('@data-entry-id').extract()
-
+            count +=1
             if gag_id:
-                last_gag_id = gag_id[0]
-                ninegag_item = GagItem()
-                ninegag_item['entry_id'] = gag_id[0]
-                ninegag_item['url'] = article.xpath('@data-entry-url').extract()[0]
-                ninegag_item['votes'] = article.xpath('@data-entry-votes').extract()[0]
-                ninegag_item['comments'] = article.xpath('@data-entry-comments').extract()[0]
-                ninegag_item['title'] = article.xpath('.//h2/a/text()').extract()[0].strip()
-                ninegag_item['img_url'] = article.xpath('.//div[1]/a/img/@src').extract()
+            	if (count != 100):
+                	last_gag_id = gag_id[0]
+                	ninegag_item = GagItem()
+                	ninegag_item['entry_id'] = gag_id[0]
+                	ninegag_item['url'] = article.xpath('@data-entry-url').extract()[0]
+                	ninegag_item['votes'] = article.xpath('@data-entry-votes').extract()[0]
+                	ninegag_item['comments'] = article.xpath('@data-entry-comments').extract()[0]
+                	ninegag_item['title'] = article.xpath('.//h2/a/text()').extract()[0].strip()
+                	ninegag_item['img_url'] = article.xpath('.//div[1]/a/img/@src').extract()
 
-                yield ninegag_item
-
+                	yield ninegag_item
+                	
+                	
+                else:
+                	break
                 
-
+        
         next_url = 'http://9gag.com/?id=%s&c=200' % last_gag_id
         yield scrapy.Request(url=next_url, callback=self.parse)	
+        print count
+
+
---
+++ SCRAPY/1-3-2016/_9gag/image_download.py
@@ -0,0 +1,38 @@
+import json,os
+path = os.getcwd()
+
+extenstion = '.jpg'
+with open("output.json") as json_file:
+    json_data = json.load(json_file)
+
+
+# for i in json_data:
+# 	image_now = i['url']
+# 	image_title = i['title']
+# 	if (extenstion in image_now):
+# 		if not os.path.exists(image_title[0:30]):
+# 			os.mkdir(image_title[0:30])
+# 			os.chdir(path+'/'+image_title[0:30])
+# 			os.system('wget ' + i['image_urls'][0] )
+# 			os.chdir(path)
+       	
+# 	else:
+# 		print "Error"
+
+count = 0
+for i in json_data:
+	image_now = i['image_url']
+	image_title = i['title']
+	count+=1
+	if (extenstion in image_now):
+		if not os.path.exists(image_title):
+			os.mkdir(image_title)
+			os.chdir(path+'/'+image_title)
+			os.system('wget ' + i['image_urls'][0] )
+			os.chdir(path)
+	# print image_title
+	# print image_now
+	# print count
+	if count == 10:
+		break
+
